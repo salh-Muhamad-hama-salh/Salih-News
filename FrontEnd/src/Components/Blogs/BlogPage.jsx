99ff19b1d2e512109e67c6af33ed24223491f97f
Salih Mohammed <Salih-Mohammed@gmail.com>
2024-08-04 17:06:58 +0300

    change pagination

--- FrontEnd/src/Components/Blogs/BlogPage.jsx
+++ FrontEnd/src/Components/Blogs/BlogPage.jsx
@@ -1,86 +1,4 @@
-// import { useQuery } from "react-query";
-// import Loading from "../Loading/Loading";
 import BlogCart from "./BlogCart";
-import { useEffect, useState } from "react";
-import Pagination from "./Pagination";
-
-function BlogPage() {
-  // const { data, isLoading, error } = useQuery("blog", async () => {
-  //   const response = await fetch("http://localhost:5000/blogs");
-  //   const data = await response.json();
-  //   return data;
-  // });
-
-  // if (isLoading) return <Loading />;
-  // if (error) return <h1>Error...</h1>;
-
-  const [data, setData] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const pageSize = 12;
-  const [selectCategory, setSelectCategory] = useState(null);
-  const [activeCategory, setActiveCategory] = useState(null);
-
-  useEffect(() => {
-    async function fechdata() {
-      let url = `http://localhost:5000/blogs?page=${currentPage}&limit=${pageSize}`;
-
-      if (selectCategory) {
-        url = `http://localhost:5000/blogs?category=${selectCategory}`;
-      }
-
-      const response = await fetch(url);
-      const data = await response.json();
-      setData(data);
-    }
-    fechdata();
-  }, [currentPage, selectCategory, pageSize]);
-
-  const handlePagechabge = (pageNumber) => {
-    setCurrentPage(pageNumber);
-  };
-
-  const handleCategory = (category) => {
-    setSelectCategory(category);
-    setCurrentPage(1);
-    setActiveCategory(category);
-  };
-
-  return (
-    <>
-      {/* page category */}
-      <div></div>
-      <div>
-        <BlogCart
-          blogs={data}
-          currentPage={currentPage}
-          selectCategory={selectCategory}
-          pageSize={pageSize}
-        />
-        {/* {data?.map((blog, index) => {
-          return (
-            <div key={index}>
-              <BlogCart blog={blog} />
-            </div>
-          );
-        })} */}
-      </div>
-      {/* pagination */}
-      <div>
-        <Pagination
-          blogs={data}
-          currentPage={currentPage}
-          pageSize={pageSize}
-          onPageChange={handlePagechabge}
-        />
-      </div>
-    </>
-  );
-}
-
-export default BlogPage;
-
-{
-  /* import BlogCart from "./BlogCart";
 import { useEffect, useState } from "react";
 import Pagination from "./Pagination";
 
@@ -108,12 +26,9 @@ function BlogPage() {
     fetchData();
   }, [currentPage, selectCategory, pageSize]);
 
-  useEffect(() => {
-    localStorage.setItem("currentPage", currentPage);
-  }, [currentPage]);
-
   const handlePageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
+    localStorage.setItem("currentPage", pageNumber);
   };
 
   const handleCategory = (category) => {
@@ -124,28 +39,27 @@ function BlogPage() {
 
   return (
     <>
-      {/* page category */
+      {/* page category */}
+      <div></div>
+      <div>
+        <BlogCart
+          blogs={data}
+          currentPage={currentPage}
+          selectCategory={selectCategory}
+          pageSize={pageSize}
+        />
+      </div>
+      {/* pagination */}
+      <div>
+        <Pagination
+          blogs={data}
+          currentPage={currentPage}
+          pageSize={pageSize}
+          onPageChange={handlePageChange}
+        />
+      </div>
+    </>
+  );
 }
-//       <div></div>
-//       <div>
-//         <BlogCart
-//           blogs={data}
-//           currentPage={currentPage}
-//           selectCategory={selectCategory}
-//           pageSize={pageSize}
-//         />
-//       </div>
-//       {/* pagination */}
-//       <div>
-//         <Pagination
-//           blogs={data}
-//           currentPage={currentPage}
-//           pageSize={pageSize}
-//           onPageChange={handlePageChange}
-//         />
-//       </div>
-//     </>
-//   );
-// }
 
-// export default BlogPage; */}
+export default BlogPage;
